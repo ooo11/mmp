463d6808bd1977ba7293ab2de07b00771355588b
ooo11 <markmizuro@gmail.com>
2024-04-01 07:15:01 +0800

    Fixed theme to black 🥷🏻

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
   description: '..left my job as a service engineer to pursue a career as a full-time web developer and to build my own business from scratch.',
 };
 
+export const viewport = {
+  width: 1,
+  themeColor: 'dark',
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,7 +26,8 @@ export default function RootLayout({
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="android-chrome-192x192" sizes="192x192" href="/android-chrome-192x192.png" />
       <link rel="android-chrome-512x512" sizes="512x512" href="/android-chrome-512x512.png" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/site.webmanifest" type="application/manifest+json" />
+
 
       <body className={inter.className}>{children}</body>
     </html>
